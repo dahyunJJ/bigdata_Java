78968d338e89e8bbcd79cae1d2a678e3dfb7326c
USER <USER@1.209.180.113>
2024-06-20 12:32:50 +0900

    <2024.06.19> java 기초

--- java_basic00/src/java_basic00/Hellojava.java
+++ java_basic00/src/java_basic00/Hellojava.java
@@ -1,13 +1,21 @@
 package java_basic00;
 
+/**
+ * 작성자 : 정다현
+ * 작성일자 : 2024.06.18
+ * 코멘트 : 자바 예제 실습 코드
+ */
+
 public class Hellojava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		// 문자열 예제
 		int test1 = 0;
 		String test2 = "안녕하세요!";
-
+	
 		System.out.println(test2);
 		
 		System.out.println("Hello Java!!");
